69ea1cb5a3c40498c3a296aa4100d02e5d371683
Gheryon <stc.fcojsr@gmail.com>
2026-02-03 23:28:38 +0100

    Actualizdo el modelo Especie Eliminada función antigua de get_especies Añadidas funciones de filtrado, store y update

--- app/Models/Especie.php
+++ app/Models/Especie.php
@@ -4,7 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Eloquent\Collection;
+use App\Services\ImageService;
 use Exception;
 
 class Especie extends Model
@@ -13,18 +16,26 @@ class Especie extends Model
 
   protected $table = 'especies';
   protected $primaryKey = 'id';
-  public $timestamps=false;
+  public $timestamps = true;
 
   protected $fillable = [
     'nombre',
+    'reino',
+    'clase_taxonomica',
+    'locomocion',
+    'organizacion_social',
     'edad',
+    'mortalidad',
     'peso',
     'altura',
     'longitud',
     'estatus',
+    'dieta',
+    'rareza',
     'anatomia',
     'alimentacion',
     'reproduccion',
+    'dimorfismo_sexual',
     'distribucion',
     'habilidades',
     'domesticacion',
@@ -33,47 +44,170 @@ class Especie extends Model
   ];
 
   /**
-   * Obtiene las especies ordenadas por nombre.
-   *
-   * @return \Illuminate\Database\Eloquent\Collection
+   * Scope para filtrar y ordenar organizaciones.
    */
-  public static function get_especies()
+  public function scopeFiltrar($query, $filtros)
   {
-    try {
-      $especies = self::select('id', 'nombre')->orderBy('nombre', 'asc')->get();
-
-      if ($especies->isEmpty()) {
-        Log::warning('No se encontraron especies.');
-        $especies = ['error' => ['error' => 'No hay especies guardadas.']];
-      }
-
-    } catch (\Exception $e) {
-      Log::error('Error al obtener especies: ' . $e->getMessage());
-      $especies = ['error' => ['error' => 'Se produjo un problema en la base de datos.']];
-    }
-    return $especies;
+    return $query->select(
+      'id',
+      'nombre',
+      'reino',
+      'peso',
+      'altura',
+      'longitud',
+      'edad',
+      'rareza',
+      'estatus',
+      'clase_taxonomica',
+      'organizacion_social',
+      'dieta',
+      'mortalidad',
+    )
+      ->when($filtros['search'] ?? null, function ($q, $search) {
+        $q->where('especies.nombre', 'LIKE', "%{$search}%");
+      })
+      ->orderBy('especies.nombre', $filtros['orden'] ?? 'asc');
   }
 
   /**
-   * Obtiene una especie por su ID.
+   * Almacena una nueva especie en la base de datos.
    *
-   * @param int $id
-   * @return \App\Models\Especie|null
+   * @param \Illuminate\Http\Request $request
+   * @return \App\Models\Especie
    */
-  public static function get_especie($id)
+  public static function store_especie($request)
   {
-    try {
-      $especie = self::findorfail($id);
-    } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $excepcion) {
-      Log::error("Especie no encontrada con ID: " . $id);
-      $especie = ['error' => ['error' => "especie no encontrada con ID: " . $id]];
-    } catch (\Illuminate\Database\QueryException $excepcion) {
-      Log::error("Especie->get_especie: Error de base de datos al obtener especie con ID: " . $id . " - " . $excepcion->getMessage());
-      $especie = ['error' => ['error' => 'Se produjo un problema en la base de datos.']];
-    } catch (\Exception $excepcion) {
-      Log::error("Especie->get_especie: Error al obtener especie con ID: " . $id . " - " . $excepcion->getMessage());
-      $especie = ['error' => ['error' => $excepcion->getMessage()]];
-    }
-    return $especie;
+    return DB::transaction(function () use ($request) {
+      // Crear registro
+      $especie = self::create([
+        'nombre' => $request->nombre,
+        'reino' => $request->reino,
+        'clase_taxonomica' => $request->clase_taxonomica,
+        'locomocion' => $request->locomocion,
+        'organizacion_social' => $request->organizacion_social,
+        'edad' => $request->edad,
+        'mortalidad' => $request->mortalidad,
+        'peso' => $request->peso,
+        'altura' => $request->altura,
+        'longitud' => $request->longitud,
+        'estatus' => $request->estatus,
+        'dieta' => $request->dieta,
+        'rareza' => $request->rareza,
+      ]);
+
+      // Procesado de campos de Summernote
+      $imageService = new ImageService();
+      $camposRichText = [
+        'anatomia' => 'anatomia',
+        'alimentacion' => 'alimentacion',
+        'reproduccion' => 'reproduccion',
+        'dimorfismo_sexual' => 'dimorfismo_sexual',
+        'distribucion' => 'distribucion',
+        'habilidades' => 'habilidades',
+        'domesticacion' => 'domesticacion',
+        'explotacion' => 'explotacion',
+        'otros' => 'otros'
+      ];
+
+      foreach ($camposRichText as $columna => $input) {
+        if ($request->filled($input)) {
+          $especie->$columna = $imageService->processSummernoteImages(
+            $request->$input,
+            "especies",
+            $especie->id
+          );
+        }
+      }
+
+      $especie->save();
+
+      return $especie;
+    });
+  }
+
+  /**
+   * Actualiza una especie existente en la base de datos.
+   *
+   * @param \Illuminate\Http\Request $request
+   * @return \App\Models\Especie
+   */
+  public function update_especie($request)
+  {
+    return DB::transaction(function () use ($request) {
+      $this->fill([
+        'nombre' => $request->nombre,
+        'reino' => $request->reino,
+        'clase_taxonomica' => $request->clase_taxonomica,
+        'locomocion' => $request->locomocion,
+        'organizacion_social' => $request->organizacion_social,
+        'edad' => $request->edad,
+        'mortalidad' => $request->mortalidad,
+        'peso' => $request->peso,
+        'altura' => $request->altura,
+        'longitud' => $request->longitud,
+        'estatus' => $request->estatus,
+        'dieta' => $request->dieta,
+        'rareza' => $request->rareza,
+      ]);
+
+      // Procesado de campos de Summernote
+      $imageService = new ImageService();
+      $camposRichText = [
+        'anatomia' => 'anatomia',
+        'alimentacion' => 'alimentacion',
+        'reproduccion' => 'reproduccion',
+        'dimorfismo_sexual' => 'dimorfismo_sexual',
+        'distribucion' => 'distribucion',
+        'habilidades' => 'habilidades',
+        'domesticacion' => 'domesticacion',
+        'explotacion' => 'explotacion',
+        'otros' => 'otros'
+      ];
+
+      foreach ($camposRichText as $columna => $input) {
+        if ($request->filled($input)) {
+          $this->$columna = $imageService->processSummernoteImages(
+            $request->$input,
+            "especies",
+            $this->id
+          );
+        }
+      }
+
+      $this->save();
+
+      return $this;
+    });
+  }
+
+  /**
+   * Elimina la especie y sus recursos asociados (imágenes).
+   * * @return void
+   */
+  public function delete_especie()
+  {
+    return DB::transaction(function () {
+      // Obtener y eliminar imágenes físicas del disco
+      $imagenes = DB::table('imagenes')
+        ->where('table_owner', 'especies')
+        ->where('owner', $this->id)
+        ->get();
+
+      foreach ($imagenes as $imagen) {
+        $path = public_path("/storage/imagenes/{$imagen->nombre}");
+        if (file_exists($path)) {
+          unlink($path);
+        }
+      }
+
+      //Limpiar registros de la tabla imagenes
+      DB::table('imagenes')
+        ->where('table_owner', 'especies')
+        ->where('owner', $this->id)
+        ->delete();
+
+      // Eliminar la especie
+      return $this->delete();
+    });
   }
 }
