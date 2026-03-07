5273a93b98347e8416c82aecfaa66510e51680a6
Gheryon <stc.fcojsr@gmail.com>
2026-03-07 15:04:28 +0100

    Renombrado el modelo TipoConstruccion para seguir la convención de nombres de Laravel.

--- app/Models/TipoConstruccion.php
+++ app/Models/TipoConstruccion.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
 
-class tipo_construccion extends Model
+class TipoConstruccion extends Model
 {
   use HasFactory;
 
